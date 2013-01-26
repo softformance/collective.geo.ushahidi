be02d4571826c570ee5895a381bb48a22b3a4d0e
Vitaliy Podoba <vitaliypodoba@gmail.com>
2013-01-26 12:11:28 +0200

    initial boilerplate

---
+++ README.txt
@@ -0,0 +1,6 @@
+Introduction
+============
+
+This package provides Ushahidi-like maps for Plone CMS.
+
+Ushahidi: http://www.ushahidi.com
---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='collective.geo.ushahidi',
+      version=version,
+      description="Ushahidi-like Maps for Plone",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='ushahidi plone maps geo kml',
+      author='Vitaliy Podoba',
+      author_email='vitaliypodoba@gmail.com',
+      url='http://github.com/collective/collective.geo.ushahidi',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective', 'collective.geo'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
